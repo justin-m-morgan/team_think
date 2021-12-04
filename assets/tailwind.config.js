2146966353d96e60cc8ae2236ee65467cd7b22e2
Justin Morgan <justin.morgan@hey.com>
2021-12-04 15:50:15 -0800

    :lipstick: (tailwind) Extend theme to include primary and accent colors

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.green,
+        accent: colors.orange,
+      },
+    },
   },
   variants: {
     extend: {},

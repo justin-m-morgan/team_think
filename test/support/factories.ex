9df3879e6d5d0e693249301bebede2bf8eef9078
Justin Morgan <justin.morgan@hey.com>
2021-12-04 15:59:23 -0800

    :white_check_mark:  Update generated test to include factories

---
+++ test/support/factories.ex
@@ -0,0 +1,49 @@
+defmodule TeamThink.Factory do
+  use ExMachina.Ecto, repo: TeamThink.Repo
+
+  alias TeamThink.Accounts.User
+
+  @min_password_length 12
+  @max_password_length 72
+
+  def valid_user_factory do
+    password = password_generator()
+
+    %User{
+      email: Faker.Internet.email(),
+      password: password,
+      hashed_password: Bcrypt.hash_pwd_salt(password)
+    }
+  end
+
+  def invalid_user_factory do
+    password = invalid_password_generator()
+
+    %User{
+      email: invalid_email_generator(),
+      password: password,
+      hashed_password: Bcrypt.hash_pwd_salt(password)
+    }
+  end
+
+  def valid_email_generator() do
+    Faker.Internet.email()
+  end
+
+  def invalid_email_generator() do
+    Faker.Lorem.words() |> Enum.join()
+  end
+
+  def password_generator(min_length \\ @min_password_length, max_length \\ @max_password_length) do
+    Faker.random_between(min_length, max_length)
+    |> Faker.random_bytes()
+  end
+
+  def invalid_password_generator() do
+    [
+      password_generator(1, @min_password_length - 1),
+      password_generator(@max_password_length + 1, 256)
+    ]
+    |> Enum.random()
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,3 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(TeamThink.Repo, :manual)
+{:ok, _} = Application.ensure_all_started(:ex_machina)

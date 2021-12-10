8a1835a87f6b46b8d259960ede54852fcd904abb
Justin Morgan <justin.morgan@hey.com>
2021-12-10 09:06:47 -0800

    :arrow_up: (tailwind) Upgrade to tailwind v3

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,18 +1,14 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     container: {
       center: true,
       padding: {
         DEFAULT: "1rem",
         sm: "1rem",
-        lg: "3rem",
-        xl: "6rem",
-        "2xl": "8rem",
       },
     },
     extend: {
@@ -25,5 +21,4 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
 };

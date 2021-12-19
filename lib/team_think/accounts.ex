c87e358db45a8e20ca7b6527e097d401b0881284
Justin Morgan <justin.morgan@hey.com>
2021-12-19 14:50:26 -0800

    :recycle:  Add preloading option to get_user

--- lib/team_think/accounts.ex
+++ lib/team_think/accounts.ex
@@ -58,7 +58,13 @@ defmodule TeamThink.Accounts do
       ** (Ecto.NoResultsError)
 
   """
-  def get_user!(id), do: Repo.get!(User, id)
+  def get_user!(id, opts \\ []) do
+    preloads = opts[:preload] || []
+
+    User
+    |> preload(^preloads)
+    |> Repo.get!(id)
+  end
 
   ## User registration
 

0c3e659eb10367f917e2cd02f590bcc595d0d0c7
Justin Morgan <justin.morgan@hey.com>
2021-12-21 08:15:36 -0800

    :bug: (deploy) Fix config per instructions on phoenix docs

--- mix.exs
+++ mix.exs
@@ -78,7 +78,6 @@ defmodule TeamThink.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": [
-        "cmd --cd assets npm run deploy",
         "esbuild default --minify",
         "phx.digest"
       ]

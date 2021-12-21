1d69e61026417285b8d907e3337a8e07ed57bc6c
Justin Morgan <justin.morgan@hey.com>
2021-12-21 07:02:11 -0800

    :wrench: (demo) Enable mailbox preview for demo purposes

--- lib/team_think_web/router.ex
+++ lib/team_think_web/router.ex
@@ -46,13 +46,13 @@ defmodule TeamThinkWeb.Router do
   #
   # Note that preview only shows emails that were sent by the same
   # node running the Phoenix server.
-  if Mix.env() == :dev do
+  # if Mix.env() == :dev do
     scope "/dev" do
       pipe_through :browser
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
-  end
+  # end
 
   ## Authentication routes
 
